82f8e58426eea2e42fbe09a8a92680cd6ebe1e78
Andrey Lukashenko <luka.andrey@gmail.com>
2016-11-26 01:03:48 +0200

    New initial project structure

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -2,8 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Comment;
-use AppBundle\Entity\Post;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,40 +11,15 @@ class BlogController extends Controller
 {
     /**
      * @Route("/", name="blog_homepage")
+     * @Method("GET")
      *
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('AppBundle:blog:index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
-    }
-
-    /**
-     * @Route("/posts/{slug}", name="blog_post")
-     * @Method("GET")
-     *
-     * @param Post $post
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function postShowAction(Post $post)
-    {
-        return $this->render('AppBundle:blog:post_show.html.twig', ['post' => $post]);
-    }
-
-    /**
-     * @Route("/comment/{slug}/new", name="comment_new")
-     * @Method("POST")
-     *
-     * @param Request $request
-     * @param Post $post
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function commentNewAction(Request $request, Post $post)
-    {
-        return $this->render('AppBundle:blog:post_show.html.twig', ['post' => $post]);
     }
 }
